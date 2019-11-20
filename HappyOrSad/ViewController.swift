b969fd17f3e1a0edafb4486f9b7cba3d5ed66acf
Russell Gordon <github@russellgordon.ca>
2019-11-20 11:16:09 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Gordon, Russell on 2019-11-20.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
